bb9ed268acebb6c6f42581017a5fc01597e7396a
bharathvaddineni <63656325+bharathvaddineni@users.noreply.github.com>
2024-04-05 10:49:26 -0500

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,14 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
 
 function Testimonials() {
   return (
-    <div>Testimonials</div>
-  )
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Testimonials</h1>
+        <p className="text-lg">Testimonials will be added soon...</p>
+      </div>
+    </div>
+  );
 }
 
-export default Testimonials
+export default Testimonials;
